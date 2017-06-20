07cb67d543c5fc8a8cf007c202525a4aff94fdfd
Ricard Solé <ricardsolecasas@gmail.com>
2017-06-20 11:15:29 +0100

    refactored code

--- bot.py
+++ bot.py
@@ -8,60 +8,51 @@ CHANNELS = {}
 EXAMPLE_COMMAND = 'do'
 BOT_NAME = 'meetbot'
 
-def make_channel_dict (channels_list):
-    channel_tuples = map(lambda (channel): (channel['id'], channel['name']), channels_list)
-    for channel_id, channel_name in channel_tuples:
-        CHANNELS[channel_id] = channel_name
+def get_id_and_name(channel):
+    return channel['id'], channel['name']
 
+def make_channel_dict(channels_list):
+    return dict(map(get_id_and_name, channels_list))
+
+def fetch_channels(client):
+    response = client.api_call('channels.list')
+    if response.get('ok'):
+        return make_channel_dict(response.get('channels'))
+    else:
+        print('Error: no channels found')
+        return {}
+
+def fetch_bot_id(client):
+    response = slack_client.api_call('users.list')
+    if response.get('ok'):
+        # retrieve all users so we can find our bot
+        users = response.get('members')
+        for user in users:
+            if 'name' in user and user.get('name') == BOT_NAME:
+                return '<@' + user.get('id') + '>'
 
 def handle_command(command, channel):
-    """
-        Receives commands directed at the bot and determines if they
-        are valid commands. If so, then acts on the commands. If not,
-        returns back what it needs for clarification.
-    """
     response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
                "* command with numbers, delimited by spaces."
     if command.startswith(EXAMPLE_COMMAND):
         response = "Sure...write some more code then I can do that!"
-    print response
     slack_client.api_call('chat.postMessage', channel='#' + channel,
                           text=response, as_user=True)
 
-
 def parse_slack_output(slack_rtm_output):
-    """
-        The Slack Real Time Messaging API is an events firehose.
-        this parsing function returns None unless a message is
-        directed at the Bot, based on its ID.
-    """
     output_list = slack_rtm_output
     if output_list and len(output_list) > 0:
         for output in output_list:
             if output and 'text' in output and AT_BOT in output['text']:
-                print output
-                # return text after the @ mention, whitespace removed
                 return output['text'].split(AT_BOT)[1].strip().lower(), \
                        output['channel']
     return None, None
 
-
 slack_client = SlackClient(environ.get('SLACK_BOT_TOKEN'))
 if slack_client.rtm_connect():
     print('Meetbot connected and running!')
-    users_call = slack_client.api_call('users.list')
-    if users_call.get('ok'):
-        # retrieve all users so we can find our bot
-        users = users_call.get('members')
-        for user in users:
-            if 'name' in user and user.get('name') == BOT_NAME:
-                AT_BOT =  '<@' + user.get('id') + '>'
-        if AT_BOT == '':
-            raise Exception
-    channel_call = slack_client.api_call('channels.list')
-    if channel_call.get('ok'):
-        channels = channel_call.get('channels')
-        make_channel_dict(channels)
+    CHANNELS = fetch_channels(slack_client)
+    AT_BOT = fetch_bot_id(slack_client)
 
     while True:
         command, channel_id = parse_slack_output(slack_client.rtm_read())
