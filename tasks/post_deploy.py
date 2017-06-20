e29fbc08b410b3c68436a1d90e1e40b80973f8cf
Ricard Solé <ricard.solecasas@gmail.com>
2017-06-20 12:53:18 +0100

    chore(bot): removes unnecessary code (#5)
    postdeploy task did not provide any useful info, just spammed the channel

--- tasks/post_deploy.py
+++
@@ -1,6 +0,0 @@
-from os import environ
-from slackclient import SlackClient
-
-sc = SlackClient(environ['SLACK_BOT_TOKEN'])
-sc.rtm_connect()
-sc.rtm_send_message('meetify', 'A new version of @meetbot has been deployed!')
